6eaeca93e50f39d35b3863e1b25fe8080289dc65
Matrix <rhailu88@gmail.com>
2022-11-18 11:04:33 -0500

    Added variables format and hours to script.js.

---
+++ assets/css/js/script.js
@@ -0,0 +1,15 @@
+// Moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+// Text hour var
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
