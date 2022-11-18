298566a650d78f7ca6bf36c3457754121448291a
Matrix <rhailu88@gmail.com>
2022-11-18 11:09:04 -0500

    Added variable interval and function for current day on script.js.

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -13,3 +13,17 @@ var fourPm = $("#16pm");
 var fivePm = $("#17pm");
 var sixPm = $("#18pm");
 var sevenPm = $("#19pm");
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+// var hourString = $(".hour").text().split(" ");
+// Date and Hour
+
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+  }, 100);
